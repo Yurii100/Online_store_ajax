f99e8c7b5bbe12269498c1f4a4ab3f1f12fc7c7e
Yury Oksamytny <C:\Users\User\AppData\Roaming\The Bat!>
2025-11-17 15:08:45 +0200

    Small changes in header.php

--- blocks/header.php
+++ blocks/header.php
@@ -1,9 +1,9 @@
 <header>
     <div class="container top-menu">
         <div class="nav">
-            <a href="../index.php">Главная</a>
-            <a href="../contacts.php">Контакты</a>
-            <a href="../about.php">Про нас</a>
+            <a href="./index.php">Главная</a>
+            <a href="./contacts.php">Контакты</a>
+            <a href="./about.php">Про нас</a>
         </div>
         <div class="tel"><i class="fa-solid fa-phone"></i>+7(000) 000 - 00 - 00</div>
     </div>
