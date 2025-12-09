f7c55dbf5e7d85868ed68b8fa882d79d803f2e5a
Yury Oksamytny <C:\Users\User\AppData\Roaming\The Bat!>
2025-12-09 19:45:44 +0200

    Changes in files about.php, contacts.php  and css/main.css  09.12.2025

--- about.php
+++ about.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Про нас</title>
-    <link rel="stylesheet" href="./css/main.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://kit.fontawesome.com/8e05dfae1d.js" crossorigin="anonymous"></script>
-</head>
-<body>
-    <?php require './blocks/header.php' ?>
-    <div class="container main">
-        <h1>Про компанию</h1>
-        <p>Здесь просто текст про компанию.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque minus, temporibus suscipit, assumenda magni, modi quam dolor excepturi nulla eius ad tempore molestias neque consequatur alias distinctio culpa debitis.</p>
-    </div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Про нас</title>
+        <link rel="stylesheet" href="./css/main.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://kit.fontawesome.com/8e05dfae1d.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <?php require './blocks/header.php' ?>
+        <div class="container main">
+            <h1>Про компанию</h1>
+            <p>Здесь просто текст про компанию.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque minus, temporibus suscipit, assumenda magni, modi quam dolor excepturi nulla eius ad tempore molestias neque consequatur alias distinctio culpa debitis.</p>
+        </div>
+    </body>
 </html>
